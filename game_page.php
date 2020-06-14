c75912fdc18174d21eff85b02a7563a51392de79
w3h2b <Zpw@37201769>
2020-06-13 22:20:02 -0700

    Finish game_page.php layout;

--- game_page.php
+++ game_page.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8" content="width=device-width, initial-scale=1">
-	<title>Where ARE MY FRIENDS?</title>
+	<title>Game Collections</title>
 	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 	<link rel="stylesheet" href="navStyle.css">
 	<style>
@@ -21,7 +21,7 @@
     margin-top: 1%;
     font-size: 400%;
     text-align: center;
-	margin-bottom: 3%;
+	margin-bottom: 1%;
     }
     .tname{
         margin : 1%;
@@ -31,6 +31,9 @@
     table, th, td {
   border: 1px solid black;
 }
+    button{
+        margin-bottom: 1%;
+    }
 	</style>
 </head>
 <body>
@@ -44,19 +47,66 @@ $(function(){
   $("#nav-placeholder").load("navbar.html");
 });
 </script>
-<!--end of Navigation bar--><?php
+<!--end of Navigation bar-->
+<div class="header">All Games</div>
 
-session_save_path("/tmp");
-session_start();
-// Connect to DB
-$conn = OCILogon("ora_reyred", "a74388869", "dbhost.students.cs.ubc.ca:1522/stu");
 
-if (!$conn) {
-    $e = oci_error();   // For oci_connect errors do not pass a handle
-    debug_to_console("Database is NOT Connected");
-    trigger_error(htmlentities($e['message']), E_USER_ERROR);
+<form method="post">
+    <select name="option">
+        <option value="Type">Type</option>
+        <option value="GameName">Game Name</option>
+        <option value="Developer">Developer</option>
+    </select>
+    <input type="text" name = "content" required="required" placeholder="Search..">
+    <button type="submit" class="btn btn-primary btn-block btn-large" name="search">Search</button>
+</form>
+
+
+<?php
+    session_save_path("/tmp");
+	session_start();
+    $conn = OCILogon("ora_reyred", "a74388869", "dbhost.students.cs.ubc.ca:1522/stu");
+    if (!$conn) {
+        $e = oci_error();   // For oci_connect errors do not pass a handle
+        debug_to_console("Database is NOT Connected");
+        trigger_error(htmlentities($e['message']), E_USER_ERROR);
+    }
+    debug_to_console("Database is Connected");
+
+
+
+
+    if (isset($_POST['search'])) {
+        searchGame();
+    }
+    else {
+        displayAllGame();
+    }
+    OCILogoff($conn);
+
+function searchGame(){
+    switch  ($_POST['option']):
+        case "Type":
+            searchByType();
+            break;
+        case "Developer":
+            searchByDev();
+            break;
+        default:
+            searchByName();
+    endswitch;
 }
-debug_to_console("Database is Connected");
+
+//TODO
+function searchByType(){
+
+}
+function searchByDev(){
+
+}
+function searchByName(){
+
+}
 
 // query for Game_uploads
 // Game page would display a game's:
@@ -64,51 +114,54 @@ debug_to_console("Database is Connected");
     // Developer Nickname
     // Price
     // Rating
-$sql = "SELECT g.Name, u.Nickname, g.Price, g.Rating
-        FROM UserID u, Game_uploads g
-        WHERE g.DevID = u.ID
-        ORDER BY g.Name";
+    function displayAllGame(){
+        global $conn;
+        $sql1 = "SELECT g.Name, u.Nickname, g.Price, g.Rating
+                FROM UserID u, Game_uploads g
+                WHERE g.DevID = u.ID
+                ORDER BY g.Name";
 
-$games = oci_parse($conn, $sql);
+        $allGames = oci_parse($conn, $sql1);
 
-// Execute sql
-$r = oci_execute($games, OCI_DEFAULT);
+        // Execute sql
+        $r = oci_execute($allGames, OCI_DEFAULT);
 
-if (!$r) {
+        if (!$r) {
 
-    $e = oci_error($stid);
-    debug_to_console($e);
-    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-}
-echo "<table>";
-echo "<tr><th>ID</th><th>Nickname</th><th>Gender</th><th>Birthday</th><th>Role</th></tr>";
-// Fetch data
+            $e = oci_error($allGames);
+            debug_to_console($e);
+            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+        }
+        echo "<table>";
 
-while ($row = OCI_Fetch_Array($games, OCI_BOTH)) {
-    echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td></tr>"; //or just use "echo $row[0]"
-    // echo $row[0];
-}
-// Store userid to server and pass to next page
-echo "</table>";
+        echo "<tr><th>Game</th><th>Developer</th><th>Price</th><th>Rating</th></tr>";
+        // Fetch data
 
-// Fetch all game data
-// $row = oci_fetch_array($games, OCI_BOTH);
-// $gName = $row[0];
-// $devName = $row[1];
-// $price = $row[2];
-// $rating = $row[3];
+        while ($row = OCI_Fetch_Array($allGames, OCI_BOTH)) {
+            if ($row[3] == null) {
+                // echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . '-' . '</td>
+                // <td><form method="post"><button type="submit" class="btn btn-primary btn-block btn-large"  value='.$row[0].' name="buy">Buy</button></form></td></tr>';
+                // echo $_POST['buy'];
+                // echo empty($_POST['buy']);
+            } else {
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . '-' . '</td>
+                <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+                </tr>';
+            }
+        }
+        // Store userid to server and pass to next page
+        echo "</table>";
 
-//close the connection i guess? Do I always have to do that?
-oci_free_statement($games);
-oci_close($conn);
+        oci_free_statement($allGames);
+    }
 
-function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
+    function debug_to_console($data) {
+        $output = $data;
+        if (is_array($output))
+            $output = implode(',', $output);
 
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
+        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    }
 
 ?>
 
