270556ab25d218454e13760661b85feba238a012
RaileyTKM <rey98426@gmail.com>
2020-06-14 23:03:44 -0700

    search functio complete

--- game_page.php
+++ game_page.php
@@ -92,75 +92,184 @@ function searchGame(){
         case "Developer":
             searchByDev();
             break;
+        case "Game Name":
+            searchByName();
+            break;
         default:
             searchByName();
     endswitch;
 }
 
-//TODO
+// !!! All searches are CASE SENSITIVE
+// TODO: autocomplete textbox
 function searchByType(){
+    global $conn;
+    $sql = "SELECT g.Name, u.Nickname, g.Price, g.Rating
+            FROM UserID u, Game_uploads g, isOf i
+            WHERE g.DevID = u.ID AND i.GID = g.GID AND i.Type = :tp_bv 
+            ORDER BY g.Name";
 
+    $found = oci_parse($conn, $sql);
+    $tp = $_POST['content'];
+    oci_bind_by_name($found, ":tp_bv", $tp);
+
+    // Execute sql
+    $r = oci_execute($found, OCI_DEFAULT);
+
+    if (!$r) {
+
+        $e = oci_error($found);
+        debug_to_console($e);
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+    echo "<table>";
+    echo "<tr><th>Game</th><th>Developer</th><th>Price</th><th>Rating</th></tr>";
+    // Fetch data
+    while ($row = OCI_Fetch_Array($found, OCI_BOTH)) {
+        if ($row[3] == null) {
+            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . '-' . '</td>
+            <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+            </tr>';
+        } else {
+            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" .$row[3]. '</td>
+            <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+            </tr>';
+        }
+    }
+    // Store userid to server and pass to next page
+    echo "</table>";
+
+    oci_free_statement($found);
 }
 function searchByDev(){
+    global $conn;
+    $sql = "SELECT g.Name, u.Nickname, g.Price, g.Rating
+            FROM UserID u, Game_uploads g
+            WHERE g.DevID = u.ID AND u.Nickname = :devn_bv
+            ORDER BY g.Name";
 
+    $found = oci_parse($conn, $sql);
+    $devn = $_POST['content'];
+    oci_bind_by_name($found, ":devn_bv", $devn);
+
+    // Execute sql
+    $r = oci_execute($found, OCI_DEFAULT);
+
+    if (!$r) {
+
+        $e = oci_error($found);
+        debug_to_console($e);
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+    echo "<table>";
+    echo "<tr><th>Game</th><th>Developer</th><th>Price</th><th>Rating</th></tr>";
+    // Fetch data
+    while ($row = OCI_Fetch_Array($found, OCI_BOTH)) {
+        if ($row[3] == null) {
+            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . '-' . '</td>
+            <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+            </tr>';
+        } else {
+            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" .$row[3]. '</td>
+            <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+            </tr>';
+        }
+    }
+    // Store userid to server and pass to next page
+    echo "</table>";
+
+    oci_free_statement($found);
 }
 function searchByName(){
+    global $conn;
+    $sql = "SELECT g.Name, u.Nickname, g.Price, g.Rating
+            FROM UserID u, Game_uploads g
+            WHERE g.DevID = u.ID AND g.Name LIKE :gn_bv
+            ORDER BY g.Name";
 
+    $found = oci_parse($conn, $sql);
+    $gn = "%". $_POST['content']. "%";
+    oci_bind_by_name($found, ":gn_bv", $gn);
+
+    // Execute sql
+    $r = oci_execute($found, OCI_DEFAULT);
+
+    if (!$r) {
+
+        $e = oci_error($found);
+        debug_to_console($e);
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+    echo "<table>";
+    echo "<tr><th>Game</th><th>Developer</th><th>Price</th><th>Rating</th></tr>";
+    // Fetch data
+    while ($row = OCI_Fetch_Array($found, OCI_BOTH)) {
+        if ($row[3] == null) {
+            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . '-' . '</td>
+            <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+            </tr>';
+        } else {
+            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" .$row[3]. '</td>
+            <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+            </tr>';
+        }
+    }
+    // Store userid to server and pass to next page
+    echo "</table>";
+
+    oci_free_statement($found);
 }
 
-// query for Game_uploads
-// Game page would display a game's:
-    // Name
-    // Developer Nickname
-    // Price
-    // Rating
-    function displayAllGame(){
-        global $conn;
-        $sql1 = "SELECT g.Name, u.Nickname, g.Price, g.Rating
-                FROM UserID u, Game_uploads g
-                WHERE g.DevID = u.ID
-                ORDER BY g.Name";
+function displayAllGame(){
+    global $conn;
+    $sql = "SELECT g.Name, u.Nickname, g.Price, g.Rating
+            FROM UserID u, Game_uploads g
+            WHERE g.DevID = u.ID
+            ORDER BY g.Name";
 
-$allGames = oci_parse($conn, $sql1);
+    $allGames = oci_parse($conn, $sql);
 
-        // Execute sql
-        $r = oci_execute($allGames, OCI_DEFAULT);
+    // Execute sql
+    $r = oci_execute($allGames, OCI_DEFAULT);
 
-if (!$r) {
+    if (!$r) {
 
-            $e = oci_error($allGames);
-            debug_to_console($e);
-            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-        }
-        echo "<table>";
-
-        echo "<tr><th>Game</th><th>Developer</th><th>Price</th><th>Rating</th></tr>";
-        // Fetch data
-
-        while ($row = OCI_Fetch_Array($allGames, OCI_BOTH)) {
-            if ($row[3] == null) {
-                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . '-' . '</td>
-                <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
-                </tr>';
-            } else {
-                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" .$row[3]. '</td>
-                <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
-                </tr>';
-            }
-        }
-        // Store userid to server and pass to next page
-        echo "</table>";
-
-        oci_free_statement($allGames);
+        $e = oci_error($allGames);
+        debug_to_console($e);
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
     }
+    echo "<table>";
 
-    function debug_to_console($data) {
-        $output = $data;
-        if (is_array($output))
-            $output = implode(',', $output);
+    echo "<tr><th>Game</th><th>Developer</th><th>Price</th><th>Rating</th></tr>";
+    // Fetch data
 
-        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    while ($row = OCI_Fetch_Array($allGames, OCI_BOTH)) {
+        if ($row[3] == null) {
+            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . '-' . '</td>
+            <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+            </tr>';
+        } else {
+            echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" .$row[3]. '</td>
+            <td><form action="purchase_game.php" method="get"><button type="submit" name="buy" value='.$row[0].' >Buy</button></form></td>
+            </tr>';
+        }
     }
+    // Store userid to server and pass to next page
+    echo "</table>";
+
+    oci_free_statement($allGames);
+}
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
 
 ?>
 
